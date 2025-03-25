a0348ca5feb7db4a6505bad110d5250d3df519da
Nuru <nuruzzamannuru7595@gmail.com>
2025-03-25 17:44:37 +0600

    tailwind latest version update

--- javascript info/vite.config.js
+++ javascript info/vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import viteClean from "vite-plugin-clean";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react()],
-})
+    base: "./",
+    build: {
+        outDir: "../",
+    },
+    plugins: [
+        react(),
+        tailwindcss(),
+        viteClean({
+            targetFiles: ["../index.html", "../assets"],
+        }),
+    ],
+});
